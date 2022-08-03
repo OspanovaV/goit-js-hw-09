c1c0ee6331204fc15ee7d541c1815faa4471df61
OspanovaV <65498262+OspanovaV@users.noreply.github.com>
2022-08-03 13:25:37 +0000

    Deploying to gh-pages from @ OspanovaV/goit-js-hw-09@85517e3edeb7370f032abfd4470b6c8b82eb1549 🚀

---
+++ 01-color-switcher.0f04773e.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.setAttribute("disabled",!1),t.addEventListener("click",(function(){timerId=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),e.removeAttribute("disabled",!0),t.setAttribute("disabled",!1)})),e.addEventListener("click",(function(){clearInterval(timerId),t.removeAttribute("disabled",!0),e.setAttribute("disabled",!1)}))}();
+//# sourceMappingURL=01-color-switcher.0f04773e.js.map
--- 01-color-switcher.f4931cb9.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){colorTimerId=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.setAttribute("disabled",!1)})),e.addEventListener("click",(function(){clearInterval(colorTimerId),console.log("Interval with id ".concat(colorTimerId," has stopped!")),t.removeAttribute("disabled",!0)}))}();
-//# sourceMappingURL=01-color-switcher.f4931cb9.js.map
